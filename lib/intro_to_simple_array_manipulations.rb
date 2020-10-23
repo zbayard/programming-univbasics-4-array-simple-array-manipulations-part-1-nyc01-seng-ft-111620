79485e575c214f0d1b5196d79e0fe0fd90b28e21
Zach Bayard <zbayard@gmail.com>
2020-10-23 13:30:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,10 +1,8 @@
-def using_push(array , string)
-  array = []
+def using_push(array, string)
   array.push(string)
 end
 
 def using_unshift(array , string)
-  array = []
   array.unshift(string)
 end
 
